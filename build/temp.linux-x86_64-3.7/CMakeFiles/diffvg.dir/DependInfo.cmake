1567fe6494c2b5b96cc9d47a0e1307565f494389
su8691 <t109598100@ntut.org.tw>
2022-09-29 16:55:42 +0800

    update

---
+++ build/temp.linux-x86_64-3.7/CMakeFiles/diffvg.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/hgfs/font/diffvg-master/diffvg-master/atomic.cpp" "/mnt/hgfs/font/diffvg-master/diffvg-master/build/temp.linux-x86_64-3.7/CMakeFiles/diffvg.dir/atomic.cpp.o"
+  "/mnt/hgfs/font/diffvg-master/diffvg-master/color.cpp" "/mnt/hgfs/font/diffvg-master/diffvg-master/build/temp.linux-x86_64-3.7/CMakeFiles/diffvg.dir/color.cpp.o"
+  "/mnt/hgfs/font/diffvg-master/diffvg-master/diffvg.cpp" "/mnt/hgfs/font/diffvg-master/diffvg-master/build/temp.linux-x86_64-3.7/CMakeFiles/diffvg.dir/diffvg.cpp.o"
+  "/mnt/hgfs/font/diffvg-master/diffvg-master/parallel.cpp" "/mnt/hgfs/font/diffvg-master/diffvg-master/build/temp.linux-x86_64-3.7/CMakeFiles/diffvg.dir/parallel.cpp.o"
+  "/mnt/hgfs/font/diffvg-master/diffvg-master/scene.cpp" "/mnt/hgfs/font/diffvg-master/diffvg-master/build/temp.linux-x86_64-3.7/CMakeFiles/diffvg.dir/scene.cpp.o"
+  "/mnt/hgfs/font/diffvg-master/diffvg-master/shape.cpp" "/mnt/hgfs/font/diffvg-master/diffvg-master/build/temp.linux-x86_64-3.7/CMakeFiles/diffvg.dir/shape.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "diffvg_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/ubuntu/anaconda3/envs/downgrade/include/python3.7m"
+  "../../pybind11/include"
+  "../../thrust"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/temp.linux-x86_64-3.7/CMakeFiles/diffvg.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/diffvg.dir/atomic.cpp.o"
+  "CMakeFiles/diffvg.dir/color.cpp.o"
+  "CMakeFiles/diffvg.dir/diffvg.cpp.o"
+  "CMakeFiles/diffvg.dir/parallel.cpp.o"
+  "CMakeFiles/diffvg.dir/scene.cpp.o"
+  "CMakeFiles/diffvg.dir/shape.cpp.o"
+  "../lib.linux-x86_64-3.7/diffvg.pdb"
+  "../lib.linux-x86_64-3.7/diffvg.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/diffvg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
